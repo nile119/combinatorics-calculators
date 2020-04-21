a7b57ca57c5bdf29277f139a55adf05c0fd36d7b
nile119 <belousov_alexander@rambler.ru>
2020-04-21 21:14:41 +0500

    make it work with python 2.x

--- 1. Exponentiation/exponentiation.py
+++ 1. Exponentiation/exponentiation.py
@@ -1,3 +1,6 @@
+
+# -*- encoding: UTF-8 -*-
+
 #
 # Возведение в степень целых чисел
 #
@@ -9,39 +12,42 @@ from os import name
 cond1=0
 print('\n * Введи целое положительное число "M":')
 while (cond1==0):
-    M=input(' M = ')
-    if ((set(M)<=(set(digits)|{'-'}))==False)|(len(M)==0):
-        net=('Нет,','Да нет же,', 'Неправильно,')
-        print(net[randrange(len(net))],'введи целое число.\n')
-    elif {'-'}<(set(M)):
-        print('Введи ПОЛОЖИТЕЛЬНОЕ целое число.\n')
-    elif (len(str(M))>4):
-        print('Длина M - < 5 знаков')
+    try:
+        M=input(' M = ')
+    except:
+        net=('Нет,','Да нет же,', 'Неправильно,') 
+        print(net[randrange(len(net))]+'введи целое число.\n')
     else:
-        cond1=1
-        
+        if M<0:
+            print 'Введи ПОЛОЖИТЕЛЬНОЕ целое число.\n'
+        elif (len(str(M))>4):
+            print('Длина M - < 5 знаков')
+        else:
+            cond1=1
 cond2=0
 print('\n * Хорошо, теперь введи целое положительное число "M":')
 while (cond2==0):
-    n=input(' n = ')
-    if ((set(n)<=(set(digits)|{'-'}))==False)|(len(n)==0):
-        net=('Нет,','Да нет же,', 'Неправильно,')
-        print(net[randrange(len(net))],'введи целое число.\n')
-    elif {'-'}<(set(n)):
-        print('Введи ПОЛОЖИТЕЛЬНОЕ целое число.\n')
-    elif (len(str(n))>4):
-        print('Длина n - < 5 знаков')
+    try:
+        n=input(' n = ')
+    except:
+        net=('Нет,','Да нет же,', 'Неправильно,') 
+        print(net[randrange(len(net))]+'введи целое число.\n')
     else:
-        cond2=1
+        if n<0:
+            print ('Введи ПОЛОЖИТЕЛЬНОЕ целое число.\n')
+        elif (len(str(n))>4):
+            print('Длина n - < 5 знаков')
+        else:
+            cond2=1
 
 r1=str(int(M)**int(n))
 
 if int(M)==0:
     print('\nНе имеет смысла при M=0.\n')
 elif len(r1)>10:
-    print('\nM в степени n ~ %s,%s * 10^%s' % (r1[0],r1[1:3],len(r1)-1))
+    print('\n%s в степени %s ~ %s,%s * 10^%s' % (M,n,r1[0],r1[1:3],len(r1)-1))
 else:
-    print('\nM в степени n = ', int(M)**int(n))
+    print('\n%s в степени %s = %d\n' % (M,n,int(M)**int(n)))
 if name=='nt':
     input()
 del n,M
